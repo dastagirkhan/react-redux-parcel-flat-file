5666d0f907b0bb87d89d11e49608e4c579163753
dastagir <dastagir@baba.local>
2018-07-04 13:04:18 +0200

    <----> Centered OBJECT <---->C

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { createStore } from "redux";
+import { createStore, bindActionCreators} from "redux";
 import { Provider, connect } from "react-redux";
 import PropTypes from "prop-types";
+import { AppContainer } from 'react-hot-loader'
 
+// Constants
+const CHANGE_NAME = 'CHANGE_NAME';
+
+// Action Creators
+const changeName = (name) => {
+	return {
+			type:CHANGE_NAME,
+			name
+	};
+}
 
 // Reducer
-const name = (state = {}, action) => {
+const reduceName = (state = {}, action) => {
   switch (action.type) {
-    case "CHANGE":
-      return Object.assign({}, { name: action.name });
+    case CHANGE_NAME:
+      return Object.assign({}, state, { name: action.name });
     default:
       return state;
   }
 };
 
 // Redux store
-const store = createStore(name, { name: "MDKD" });
+const initialState = { name: "MDK" }
+const store = (() => {
+   if (module.hot && (process.env.NODE_ENV === 'development') && window.store) {    
+      return window.store;
+   }
+   
+   const store = createStore(reduceName, initialState);
+   if (module.hot && process.env.NODE_ENV === 'development') { 
+      window.store = store;
+   }
+   return store;
+})()
+
 
 // React Component
 class Greeting extends React.Component {
@@ -32,7 +55,7 @@ class Greeting extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.handleChange} />
+        <input type="text" placeholder="type something" onChange={this.handleChange} />
         <br />
         Hello, {this.props.name}
       </div>
@@ -45,28 +68,27 @@ Greeting.propTypes = {
   name: PropTypes.string.isRequired
 };
 
+// mapStateToProps() is a utility which helps your component get updated state(which is updated by some other components),
+const mapStateToProps = state => {
+	return {
+		name: state.name
+	}
+}
+// mapDispatchToProps() is a utility which will help your component to fire an action event (dispatching action which may cause change of application state)
+const mapDispatchToProps = dispatch => {
+	return bindActionCreators({changeName},dispatch);
+}
 // Connecting React component with Redux returns a instance of that component
-const GreetingConnection = connect(
-  state => {
-    return {
-      name: state.name
-    };
-  },
-  dispatch => {
-    return {
-      changeName: name => {
-        dispatch({ type: "CHANGE", name });
-      }
-    };
-  }
-)(Greeting);
+const GreetingConnection = connect(mapStateToProps,mapDispatchToProps)(Greeting);
 
 // Rendering,
-function renderApp(){
-  ReactDom.render(
-    <Provider store={store}>
-      <GreetingConnection />
-    </Provider>,
+function renderApp(){  
+  ReactDom.render(    
+    <AppContainer>
+      <Provider store={store}>       
+            <GreetingConnection />      
+      </Provider>
+      </AppContainer>,
     document.getElementById("mount")
   );
 }
@@ -74,6 +96,6 @@ renderApp();
    
 // Hot Module Replacement
 if (module.hot) {
-  module.hot.accept(renderApp);
+    module.hot.accept(renderApp);  
 }
   
